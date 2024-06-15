0ad24c19698c5f9e33286c693d209ede553cdae1
RobottiFranco <franco.robotti0@gmail.com>
2024-06-15 15:50:34 -0300

    init maven proyect

---
+++ src/test/java/com/ada/olimpiadas/OlimpiadasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.olimpiadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpiadasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
