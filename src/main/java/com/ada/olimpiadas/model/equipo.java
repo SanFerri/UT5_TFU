d0be763d51652f05742038e116705219ca77a4c0
santiago.ferraro <santiago.ferraro@correo.ucu.edu.uy>
2024-06-15 17:00:44 -0300

    inicial

--- src/main/java/com/ada/olimpiadas/model/equipo.java
+++ src/main/java/com/ada/olimpiadas/model/equipo.java
@@ -1,5 +1,21 @@
 package com.ada.olimpiadas.model;
 
-public class equipo {
+
+public class Equipo {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasenia;
+    private String cedula;
+    private String contacto;
+    private Participante[] participantes;
     
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/ada/olimpiadas/model/juez.java
+++ src/main/java/com/ada/olimpiadas/model/juez.java
@@ -1,5 +1,68 @@
 package com.ada.olimpiadas.model;
 
-public class juez {
+public class Juez {
+   
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasenia;
+    private String cedula;
+    private String contacto;
     
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getCorreo() {
+        return correo;
+    }
+    
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    public String getContrasenia() {
+        return contrasenia;
+    }
+    
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+    
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
 }
