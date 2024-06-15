60f6c4cfe40ff2ba91d5ea6ced395d078a2de783
RobottiFranco <franco.robotti0@gmail.com>
2024-06-15 17:34:22 -0300

    init

--- src/main/java/com/ada/olimpiadas/model/disciplina.java
+++ src/main/java/com/ada/olimpiadas/model/disciplina.java
@@ -1,5 +1,20 @@
 package com.ada.olimpiadas.model;
 
+import java.util.ArrayList;
+
 public class Disciplina {
-    
+        private String nombre;
+    private ArrayList<Modalidad> modalidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Modalidad> getModalidad() {
+        return modalidad;
+    }
 }
